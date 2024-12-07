d7dfd041f897618b1d73dc72aa71e36427a17821
Tom_Cat <maoy896@gmail.com>
2024-12-07 18:11:06 +0800

    complete the problem 438, learn more about sliding windows

--- 438.找到字符串中所有字母异位词.cpp
+++ 438.找到字符串中所有字母异位词.cpp
@@ -12,7 +12,32 @@ using namespace std;
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        
+        vector<int> result;
+        int length = p.size();
+        array<int, 26> p_letters{};
+        array<int, 26> s_letters{};
+
+        for (char c : p) {
+            p_letters[c - 'a']++;
+        }
+
+        for (int right = 0; right < s.size(); right++) {
+            s_letters[s[right] - 'a']++;
+
+            int left = right - length + 1;
+            if (left < 0) {
+                continue;
+            }
+
+            if (p_letters == s_letters) {
+                result.push_back(left);
+            }
+
+            s_letters[s[left] - 'a']--;
+        }
+
+
+        return result;
     }
 };
 // @lc code=end
