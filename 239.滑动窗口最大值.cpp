ca9e9bf597d259e0b317e296b098f3bbfb2aab62
Tom_Cat <maoy896@gmail.com>
2024-12-09 11:51:49 +0800

    complete the problem 239 with two way

--- 239.滑动窗口最大值.cpp
+++ 239.滑动窗口最大值.cpp
@@ -9,10 +9,37 @@
 
 using namespace std;
 
+/* 单调队列方法 */
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        return vector<int>();
+        vector<int> max_vec;
+        deque<int> doubleq;
+
+        for (int i = 0; i < nums.size(); i++) {
+            /** if the front element has pass the k length 
+             * just pop it
+             */
+            while(!doubleq.empty() && i - doubleq.front() >= k) {
+                doubleq.pop_front();
+            }
+
+            /** if the last element is smaller than current 
+             * just pop the last one
+             */
+            while(!doubleq.empty() && nums[i] > nums[doubleq.back()]) {
+                doubleq.pop_back();
+            }
+
+            doubleq.push_back(i);
+
+            if (i >= k - 1) {
+                max_vec.push_back(nums[doubleq.front()]); // push to the answer
+            }
+        }
+
+
+        return max_vec;
     }
 };
 // @lc code=end
