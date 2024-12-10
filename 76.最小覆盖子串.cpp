e4360214ded5bcd12c57754e4c68950d8dea0744
Tom_Cat <maoy896@gmail.com>
2024-12-10 10:40:42 +0800

    writing sparse table note to the README

---
+++ 76.最小覆盖子串.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=76 lang=cpp
+ *
+ * [76] 最小覆盖子串
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int min_length = t.size();
+    }
+};
+// @lc code=end
+
