c304c6de1cde7a8eec64996d5dd135525775b12a
Tom_Cat <maoy896@gmail.com>
2024-12-15 20:37:21 +0800

    complete problem 19 and 24, add .gitignore file

--- 19.删除链表的倒数第-n-个结点.cpp
+++ 19.删除链表的倒数第-n-个结点.cpp
@@ -16,31 +16,77 @@
  * };
  */
 #include <bits/stdc++.h>
-#include <unordered_set>
 
 using namespace std;
 
 class Solution {
 public:
-    struct ListNode {
-        int val;
-        ListNode *next;
-        ListNode() : val(0), next(nullptr) {}
-        ListNode(int x) : val(x), next(nullptr) {}
-        ListNode(int x, ListNode *next) : val(x), next(next) {}
-    };
+    // struct ListNode {
+    //     int val;
+    //     ListNode *next;
+    //     ListNode() : val(0), next(nullptr) {}
+    //     ListNode(int x) : val(x), next(nullptr) {}
+    //     ListNode(int x, ListNode *next) : val(x), next(next) {}
+    // };
 
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         vector<ListNode*> address;
+        ListNode* result;
+        if (head -> next == nullptr && n == 1) return nullptr;
 
-        auto* ptr = head;
+        ListNode* ptr = head;
 
         while (ptr != nullptr) {
             address.push_back(ptr);
             ptr = ptr -> next;
         }
 
-        return nullptr;
+        int length = address.size();
+        int ignore_num = length - n;
+
+
+        if (ignore_num == 0) {
+            result = new ListNode(address[1] -> val);
+            ptr = result;
+            for (int i = 2; i < length; i++) {
+                ptr -> next = new ListNode(address[i] -> val);
+                ptr = ptr -> next;
+            }
+            return result;
+        } else {
+            result = new ListNode(address[0] -> val);
+            ptr = result;
+        }
+
+        for (int i = 1; i < length; i++) {
+            if (i == ignore_num) continue;
+            ptr -> next = new ListNode(address[i] -> val);
+            ptr = ptr -> next;
+        }
+
+        return result;
     }
+
 };
+
+// int main () {
+//     Solution s;
+//     Solution::ListNode* head = new Solution::ListNode(1);
+//     Solution::ListNode* ptr = head;
+//     ptr -> next = new Solution::ListNode(2);
+//     ptr = ptr -> next;
+//     ptr -> next = new Solution::ListNode(3);
+//     ptr = ptr -> next;
+//     ptr -> next = new Solution::ListNode(4);
+//     ptr = ptr -> next;
+//     ptr -> next = new Solution::ListNode(5);
+//     ptr = ptr -> next;
+
+//     Solution::ListNode* result = s.removeNthFromEnd(head, 2);
+
+//     while (result != nullptr) {
+//         cout << result -> val << endl;
+//         result = result -> next;
+//     }
+// }
 // @lc code=end
---
+++ 24.两两交换链表中的节点.cpp
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=cpp
+ *
+ * [24] 两两交换链表中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    // struct ListNode {
+    //     int val;
+    //     ListNode *next;
+    //     ListNode() : val(0), next(nullptr) {}
+    //     ListNode(int x) : val(x), next(nullptr) {}
+    //     ListNode(int x, ListNode *next) : val(x), next(next) {}
+    // };
+
+    ListNode* swapPairs(ListNode* head) {
+        if (head == nullptr || head -> next == nullptr) return head;
+
+        // head -> next -> next = swapPairs(head -> next -> next -> next);
+
+        ListNode* current = head -> next;
+        ListNode* temp = current -> next;
+
+        current -> next = head;
+
+        head -> next = swapPairs(temp);
+
+
+        return current;
+    }
+
+};
+// @lc code=end
