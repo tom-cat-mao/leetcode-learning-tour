ec8360a5581e942b4d40c1aede8b5a624b61f7b1
Tom_Cat <maoy896@gmail.com>
2024-12-18 10:53:32 +0800

    complete problem 94

---
+++ 94.二叉树的中序遍历.cpp
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=cpp
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// struct TreeNode {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// };
+
+class Solution {
+public:
+    vector<int> result;
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        if (root == nullptr) return vector<int>();
+        gothrough(root);
+        return result;
+    }
+
+    void gothrough(TreeNode* root) {
+        if (root == nullptr) return;
+
+        gothrough(root -> left);
+        result.push_back(root -> val);
+        gothrough(root -> right);
+    }
+};
+// @lc code=end
