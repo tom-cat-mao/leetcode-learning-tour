e8fa60b86a8c21ecfdc87ce8f0485cc30dd7007b
Tom_Cat <maoy896@gmail.com>
2024-12-18 11:20:13 +0800

    complete problem 104

--- 104.二叉树的最大深度.cpp
+++ 104.二叉树的最大深度.cpp
@@ -16,11 +16,31 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// struct TreeNode {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// };
+
 class Solution {
 public:
+
     int maxDepth(TreeNode* root) {
-        
+        if (root == 0) return 0;
+
+        int leftDepth = maxDepth(root -> left);
+        int rightDepth = maxDepth(root -> right);
+
+        return max(leftDepth, rightDepth) + 1;
     }
+
+
 };
 // @lc code=end
-
