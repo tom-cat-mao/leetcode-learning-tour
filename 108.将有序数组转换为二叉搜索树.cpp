1ce325424baf1683ebe2e1fdaf8cb0aa19385364
Tom_Cat <maoy896@gmail.com>
2024-12-19 16:45:09 +0800

    complete problem 108

---
+++ 108.将有序数组转换为二叉搜索树.cpp
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode.cn id=108 lang=cpp
+ *
+ * [108] 将有序数组转换为二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+// struct TreeNode {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// };
+
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+        return helper(nums, 0, nums.size() - 1);
+    }
+
+    TreeNode* helper(vector<int>& nums, int left, int right) {
+        if (left > right) return nullptr;
+
+        int middle = (left + right) / 2;
+
+        TreeNode* root = new TreeNode(nums[middle]);
+
+        root -> left = helper(nums, left, middle - 1);
+        root -> right = helper(nums, middle + 1, right);
+
+        return root;
+    }
+};
+
+// int main() {
+//     Solution s;
+//     vector<int> nums = {-10, -3, 0, 5, 9};
+//     TreeNode* result = s.sortedArrayToBST(nums);
+
+//     return 0;
+// }
+// @lc code=end
