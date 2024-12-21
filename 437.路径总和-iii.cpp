f9afcaf15c9f4294c26cfecc835a915bb0381b7f
Tom_Cat <maoy896@gmail.com>
2024-12-21 22:47:13 +0800

    add problem 437

---
+++ 437.路径总和-iii.cpp
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=437 lang=cpp
+ *
+ * [437] 路径总和 III
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    int pathSum(TreeNode* root, int targetSum) {
+        stack<TreeNode*> mystack;
+        TreeNode* ptr = root;
+        int sum = 0;
+
+        while(!mystack.empty() && ptr != nullptr) {
+            while(ptr != nullptr) {
+                mystack.push(ptr);
+                ptr = ptr -> left;
+            }
+        }
+
+        return 0;
+    }
+};
+// @lc code=end
