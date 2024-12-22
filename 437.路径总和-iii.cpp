ccd317695d34be58b756860ede0713b454f090af
Tom_Cat <maoy896@gmail.com>
2024-12-22 20:33:47 +0800

    complete problem 437

--- 437.路径总和-iii.cpp
+++ 437.路径总和-iii.cpp
@@ -20,30 +20,36 @@
 
 using namespace std;
 
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
-};
+// struct TreeNode {
+//     int val;
+//     TreeNode *left;
+//     TreeNode *right;
+//     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+// };
 
 class Solution {
 public:
     int pathSum(TreeNode* root, int targetSum) {
-        stack<TreeNode*> mystack;
-        TreeNode* ptr = root;
-        int sum = 0;
+        if (root == nullptr) return 0;
+        long count = rootSum(root, targetSum);
 
-        while(!mystack.empty() && ptr != nullptr) {
-            while(ptr != nullptr) {
-                mystack.push(ptr);
-                ptr = ptr -> left;
-            }
-        }
+        count += pathSum(root -> left, targetSum);
+        count += pathSum(root -> right, targetSum);
+        return count;
+    }
 
-        return 0;
+    int rootSum(TreeNode* root, long targetSum) {
+        if (root == nullptr) return 0;
+
+        int count = 0;
+        if (root -> val == targetSum) count++;
+
+        count += rootSum(root -> left, targetSum - root -> val);
+        count += rootSum(root -> right, targetSum - root -> val);
+
+        return count;
     }
 };
 // @lc code=end
