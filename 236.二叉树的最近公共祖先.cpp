4d8030bbe059e3d368f3cde54cdf238a6943f264
Tom_Cat <maoy896@gmail.com>
2024-12-23 13:54:29 +0800

    add some explanation and note to problem 236

--- 236.二叉树的最近公共祖先.cpp
+++ 236.二叉树的最近公共祖先.cpp
@@ -29,14 +29,14 @@ using namespace std;
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (root == nullptr || root == p || root == q) return root;
+        if (root == nullptr || root == p || root == q) return root; // 如果到达叶子节点或找到p或找到q返回当前节点
 
         TreeNode* left = lowestCommonAncestor(root -> left, p, q);
         TreeNode* right = lowestCommonAncestor(root -> right, p, q);
 
-        if (left != nullptr && right != nullptr) return root;
+        if (left != nullptr && right != nullptr) return root; // 当左右均找到时，返回当前节点
 
-        return left != nullptr ? left : right;
+        return left != nullptr ? left : right; // 当左侧未找到时返回右侧节点
     }
 
 };
