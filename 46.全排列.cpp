ea5a932b770cc4eb7c8736f6e0efb800fe2dc347
Tom_Cat <maoy896@gmail.com>
2024-12-26 14:34:24 +0800

    add problem 46

---
+++ 46.全排列.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=46 lang=cpp
+ *
+ * [46] 全排列
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        return vector<vector<int>>();
+    }
+};
+// @lc code=end
+
