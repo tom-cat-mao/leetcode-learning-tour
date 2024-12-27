73008c7ec0e2c07a8dccd46f3bdf05d53e396bf5
Tom_Cat <maoy896@gmail.com>
2024-12-27 10:08:10 +0800

    complete problem 46

--- 46.全排列.cpp
+++ 46.全排列.cpp
@@ -6,14 +6,32 @@
 
 // @lc code=start
 #include <bits/stdc++.h>
+#include <utility>
+#include <vector>
 
 using namespace std;
 
 class Solution {
 public:
     vector<vector<int>> permute(vector<int>& nums) {
-        return vector<vector<int>>();
+        vector<vector<int>> result;
+        getPermutations(nums, 0, result);
+
+        return result;
     }
+
+    void getPermutations(vector<int>& array, int start, vector<vector<int>>& result) {
+        if (start == array.size() - 1) {
+            result.push_back(array);
+            return;
+        }
+
+        for (int i = start; i < array.size(); i++) {
+            swap(array.at(start), array.at(i));
+            getPermutations(array, start + 1, result);
+            swap(array.at(start), array.at(i));
+        }
+    }
+
 };
 // @lc code=end
-
