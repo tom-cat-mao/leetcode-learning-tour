b723d781973e2ef691a0bf5a38aa161605e2110f
Tom_Cat <maoy896@gmail.com>
2024-12-30 10:07:18 +0800

    add problem 51

---
+++ 51.n-皇后.cpp
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=51 lang=cpp
+ *
+ * [51] N 皇后
+ */
+
+// @lc code=start
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        return vector<vector<string>>();
+    }
+};
+// @lc code=end
+
