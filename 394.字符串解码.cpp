710c03a57ed77ebfe6164362ec4f25d604082547
Tom_Cat <maoy896@gmail.com>
2025-01-03 09:59:27 +0800

    complete problem 394 in a interesting way. i will add the note later

--- 394.字符串解码.cpp
+++ 394.字符串解码.cpp
@@ -6,14 +6,33 @@
 
 // @lc code=start
 #include <bits/stdc++.h>
+#include <cctype>
 
 using namespace std;
 
 class Solution {
 public:
     string decodeString(string s) {
-        return string();
+        string ans;
+        stack<pair<int, int>> stk;
+        int count = 0;
+
+        for (char c: s) {
+            if (isdigit(c)) count = 10 * count + (c - '0');
+            else if (c == '[') {
+                stk.push({count, ans.size()});
+                count = 0;
+            } else if (isalpha(c)) ans.push_back(c);
+            else if (c == ']') {
+                int num = stk.top().first;
+                string str = ans.substr(stk.top().second, ans.size() - stk.top().second);
+                for (int i = 0; i < num - 1; i++) ans.append(str);
+                stk.pop();
+            }
+        }
+
+        return ans;
     }
+
 };
 // @lc code=end
-
