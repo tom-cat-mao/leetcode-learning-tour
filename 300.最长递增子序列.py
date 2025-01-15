f7e474bf0b2b2760f90b871558032ebe525c4e26
Tom_Cat <maoy896@gmail.com>
2025-01-15 22:49:53 +0800

    add solution for problem 300: 最长递增子序列 in Python

---
+++ 300.最长递增子序列.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode.cn id=300 lang=python3
+#
+# [300] 最长递增子序列
+#
+
+# @lc code=start
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        g = []
+        for x in nums:
+            j = bisect_left(g, x)
+            if j == len(g):
+                g.append(x)
+            else:
+                g[j] = x
+        return len(g)
+        
+# @lc code=end
+
